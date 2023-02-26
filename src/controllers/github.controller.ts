556b182bcd0c54fd79da2eef293f1df54c7789fa
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2023-02-27 02:17:44 +0700

    build: webhook

--- src/controllers/github.controller.ts
+++ src/controllers/github.controller.ts
@@ -33,7 +33,7 @@ class GithubHookController implements Controller {
     this.path = ConstantAPI.HOOK
     this.router = Router()
     this.webhookHandler = new GithubWebhook({
-      events: ['push', 'workflow_run', 'workflow_job', 'in_progress']
+      events: ['push', 'workflow_run', 'workflow_job', 'in_progress', 'completed']
     })
 
     this.initialiseRoutes()
